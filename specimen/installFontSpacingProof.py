075fefe255c5ff3f6127da63907a554401da3c09
Max Esnée <contact@max-esnee.com>
2020-05-05 10:29:40 +0200

    Add date and fontName

--- specimen/installFontSpacingProof.py
+++ specimen/installFontSpacingProof.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
+fontURL = '../otf/roman.otf'
 
 txt = FormattedString()
-txt.font('/otf/roman.otf')
+txt.font(fontURL)
 # txt.font("Times")
 txt.fallbackFont("Times-Italic")
 txt.fontSize(60)
+
 
 
 
@@ -28,5 +32,15 @@ for glyphName in txt.listFontGlyphNames():
 while txt:
     newPage()
     txt = textBox(txt, (10, 10, width()-20, height()-20))
+
+
+# Add the date and name of the fotn
+psName = font(fontURL)
+text(psName, (40, 40))
+date = datetime.today().strftime("%d/%m/%Y")
+font("Arial")
+fontSize(11)
+text(date,(width()-40,40), align="right")
+
 
 
