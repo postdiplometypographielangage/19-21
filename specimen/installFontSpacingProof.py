8fbdf6b5db0b2068944fcfbddbd672b7893c279c
Max Esnée <contact@max-esnee.com>
2020-05-05 11:02:25 +0200

    Update specimen layout

--- specimen/installFontSpacingProof.py
+++ specimen/installFontSpacingProof.py
@@ -6,23 +6,19 @@ txt = FormattedString()
 txt.font(fontURL)
 # txt.font("Times")
 txt.fallbackFont("Times-Italic")
-txt.fontSize(60)
-
-
-
-
-# font("Helvetica")
-# txt += "Hello Amiens"
-
-# help(txt)
-# print(txt.listFontGlyphNames())
+txt.fontSize(240)
+txt.lineHeight(240)
 
 txt.appendGlyph(*txt.listFontGlyphNames())
 
+txt.fontSize(24)
+txt.lineHeight(28)
 txt += "\n" * 2
 
+txt += "encaissasses seize cas cane ni nies naine cessasses cessas sens an aise assainissez zen scia sains cannisses sana sic sis seize cannisse assainissez encaissasses encaissiez encaissassiez encens essais aines encaissez encaissassiez casasses scies cas cas incises zinc an ans incisasse sens sise sis canna canisse saines aines cessa assainisses naissais inca encan cannes cassas anis ces encaisse nazies ac assassiniez science aisance sic sciences canne zinc naisses canines cas naine cas encaissiez se saisie assainissez cc encaissais sans incisassiez sain saisissez cessassiez se sic ceci anse ac aines ananas saisissiez nia encaissez aise zizanies saisies nain ci ai cannas nez saisi nias ananas naine"
 
-txt.fontSize(15)
+
+txt += "\n" * 3
 
 for glyphName in txt.listFontGlyphNames():
     txt.appendGlyph("n", "n", glyphName, "n", "n", "space")
@@ -31,21 +27,20 @@ for glyphName in txt.listFontGlyphNames():
 
 
 while txt:
-    newPage()
-    txt = textBox(txt, (10, 10, width()-20, height()-20))
+    newPage("A4Landscape")
+    txt = textBox(txt, (40, 40, width()-80, height()-80))
+    
+    # Add the date and name of the font
+    psName = font(fontURL)
+    font("Arial")
+    fontSize(11)
+    text(psName, (40, 40))
+    date = datetime.today().strftime("%d/%m/%Y")
+    font("Arial")
+    fontSize(11)
+    text(date,(width()-40,40), align="right")
 
 
-font(fontURL, 24)
-textBox("encaissasses seize cas cane ni nies naine cessasses cessas sens an aise assainissez zen scia sains cannisses sana sic sis seize cannisse assainissez encaissasses encaissiez encaissassiez encens essais aines encaissez encaissassiez casasses scies cas cas incises zinc an ans incisasse sens sise sis canna canisse saines aines cessa assainisses naissais inca encan cannes cassas anis ces encaisse nazies ac assassiniez science aisance sic sciences canne zinc naisses canines cas naine cas encaissiez se saisie assainissez cc encaissais sans incisassiez sain saisissez cessassiez se sic ceci anse ac aines ananas saisissiez nia encaissez aise zizanies saisies nain ci ai cannas nez saisi nias ananas naine ancien cc naissance sain encans zen incisa scie encaissassiez cc sana assainissez nain canisses nazie ai ", (40, height()-500, width()-80, 200), align="left")
-  
-# Add the date and name of the font
-psName = font(fontURL)
-font("Arial")
-fontSize(11)
-text(psName, (40, 40))
-date = datetime.today().strftime("%d/%m/%Y")
-font("Arial")
-fontSize(11)
-text(date,(width()-40,40), align="right")
 
 
+ 
