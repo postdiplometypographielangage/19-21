e174741c84d32382d0319e40d2c4ab3c3c4eab69
Max Esnée <contact@max-esnee.com>
2020-05-05 11:06:37 +0200

    Update installFontSpacingProof.py

--- specimen/installFontSpacingProof.py
+++ specimen/installFontSpacingProof.py
@@ -4,21 +4,23 @@ fontURL = '../otf/roman.otf'
 
 txt = FormattedString()
 txt.font(fontURL)
-# txt.font("Times")
-txt.fallbackFont("Times-Italic")
+
+# Glyphset
 txt.fontSize(240)
-txt.lineHeight(240)
+txt.lineHeight(220)
 
 txt.appendGlyph(*txt.listFontGlyphNames())
 
+# Words
 txt.fontSize(24)
 txt.lineHeight(28)
 txt += "\n" * 2
 
-txt += "encaissasses seize cas cane ni nies naine cessasses cessas sens an aise assainissez zen scia sains cannisses sana sic sis seize cannisse assainissez encaissasses encaissiez encaissassiez encens essais aines encaissez encaissassiez casasses scies cas cas incises zinc an ans incisasse sens sise sis canna canisse saines aines cessa assainisses naissais inca encan cannes cassas anis ces encaisse nazies ac assassiniez science aisance sic sciences canne zinc naisses canines cas naine cas encaissiez se saisie assainissez cc encaissais sans incisassiez sain saisissez cessassiez se sic ceci anse ac aines ananas saisissiez nia encaissez aise zizanies saisies nain ci ai cannas nez saisi nias ananas naine"
+txt += "encaissasses seize cas cane ni nies naine cessasses cessas sens an aise assainissez zen scia sains cannisses sana sic sis seize cannisse assainissez encaissasses encaissiez encaissassiez encens essais aines encaissez encaissassiez casasses scies cas cas incises zinc an ans incisasse sens sise sis canna canisse saines aines cessa assainisses naissais inca encan cannes cassas anis ces encaisse nazies ac assassiniez science aisance sic sciences canne zinc naisses canines cas naine cas encaissiez se saisie assainissez cc encaissais sans incisassiez sain saisissez cessassiez se sic ceci anse ac aines ananas saisissiez nia encaissez aise zizanies saisies nain ci ai cannas nez saisi nias ananas naine ancien cc naissance"
 
 
-txt += "\n" * 3
+# Spacing
+txt += "\n" * 2
 
 for glyphName in txt.listFontGlyphNames():
     txt.appendGlyph("n", "n", glyphName, "n", "n", "space")
@@ -28,7 +30,7 @@ for glyphName in txt.listFontGlyphNames():
 
 while txt:
     newPage("A4Landscape")
-    txt = textBox(txt, (40, 40, width()-80, height()-80))
+    txt = textBox(txt, (40, 40, width()-80, height()-100))
     
     # Add the date and name of the font
     psName = font(fontURL)
